ec5a9d763e12c16c8edc73887dc09cd9241ca0ee
FThelin <fredrikthelin82@gmail.com>
2019-11-27 10:26:35 +0100

    Basic layout and design done

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ let jan, mar, may, jul, aug, okt, dec;
 jan = mar = may = jul = aug = okt = dec = 31;
 
 for(let i = 1; i < 32; i++){
-    days =  `<div>${i}</div>`;
+    days =  `<div id="button-${i}">${i}</div>`;
     const x = document.querySelector('.calender');
 
     x.innerHTML += days;
