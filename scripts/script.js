2b10b6ee4bbd5ead768458223a4a038629442f8f
FThelin <fredrikthelin82@gmail.com>
2019-11-27 11:27:40 +0100

    Co-authored-by: Anton Öhman <anton.ohman@hotmail.com> Co-authored-by: Simon Beijer <s_beijer@hotmail.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,70 @@
-let jan, mar, may, jul, aug, okt, dec;
-jan = mar = may = jul = aug = okt = dec = 31;
+window.addEventListener('load', init);
 
-for(let i = 1; i < 32; i++){
-    days =  `<div id="button-${i}">${i}</div>`;
-    const x = document.querySelector('.calender');
+const decrement = document.querySelector('.decrement-month');
 
-    x.innerHTML += days;
+function init(){    
+    generateDays();
+    getDate();
 }
+
+function getDate() {
+    const span = document.querySelector('section span');
+    const date = new Date();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    
+    switch (month) {
+        case 0:
+            span.innerText = `Januari ${year}`;
+            break;
+        case 1:
+            span.innerText = `Februari ${year}`;
+            break;
+        case 2:
+            span.innerText = `Mars ${year}`;
+            break;
+        case 3:
+            span.innerText = `April ${year}`;
+            break;
+        case 4:
+            span.innerText = `Maj ${year}`;
+            break;
+        case 5:
+            span.innerText = `Juni ${year}`;
+            break;
+        case 6:
+            span.innerText = `Juli ${year}`;
+            break;
+        case 7:
+            span.innerText = `Augusti ${year}`;
+            break;
+        case 8:
+            span.innerText = `September ${year}`;
+            break;
+        case 9:
+            span.innerText = `Oktober ${year}`;
+            break;
+        case 10:
+            span.innerText = `November ${year}`;
+            break;
+        case 11:
+            span.innerText = `December ${year}`;
+            break;
+        default:
+            span.innerText = `error`;
+    }
+    
+}
+
+function generateDays() {
+    let jan, mar, may, jul, aug, okt, dec;
+    jan = mar = may = jul = aug = okt = dec = 31;
+
+    for(let i = 1; i < 32 ; i++){
+        days =  `<div id="button-${i}">${i}</div>`;
+        const x = document.querySelector('.calender');
+
+        x.innerHTML += days;
+    }
+}
+
