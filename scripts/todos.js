57da523e324d19710065fc5cb024a634683bb4c9
FThelin <fredrikthelin82@gmail.com>
2019-12-03 12:44:45 +0100

    Issue 12 show todos depending on selected day

--- scripts/todos.js
+++ scripts/todos.js
@@ -1,8 +1,11 @@
 class Todos {
-    constructor(year, month, day,){
+    constructor(year, month, day, text, timeFrom, timeTo){
         this.year = year;
         this.month = month;
         this.day = day;
+        this.text = text;
+        this.timeFrom = timeFrom;
+        this.timeTo = timeTo;
     }   
 
     static todoList(todo){
@@ -12,58 +15,30 @@ class Todos {
     }    
 }
 
-const addTodo = () => {   
-    const todoContent = document.createElement('div');
-    todoContent.innerHTML = `<input type="text" placeholder="Todo..." maxlength="18"><div id="time">Tid:<input type="time"></input> - <input type="time"></input></div><div id="icons"><i class="fas fa-times"></i><i class="fas fa-check"></i></div>`;
-    $('.todos').append(todoContent);
-    $(todoContent).on('click', markDone); 
-    addTodoEventListeners();
-    saveTodo();    
+const clearForm = () => {
+    $('#text').val("");
+    $('#from').val("");
+    $('#to').val("");
 }
 
-const saveTodo = () => {
-    let day = $('.date .day-number').text();
-    let month = $('.date .month').text();  
-    let year = $('.date .year').text();
-    let date = day + month + year;     
-    let todo = new Todos(year, month, day);
-    let todoList = [];
-    let todoInStorage;
+const showTodos = () => {
     
-    if(localStorage.getItem(date)){
-        let storage = JSON.parse(localStorage.getItem(date));
-        for (let i = 0; i < storage.length; i++){
-            todoInStorage = storage[i];
-            todoList.push(todoInStorage);
-            
+    let day = $('.day-number').text();
+    let month = $('.month').text();
+    let year = $('.year').text(); 
+    let getTodos; 
+
+    keys = Object.keys(localStorage);        
+    for(let i = 0; i < keys.length; i++) {
+        getTodos = JSON.parse(localStorage.getItem(keys[i]));
+        for(let j = 0; j < getTodos.length; j++){
+            if (getTodos[j].day == day && getTodos[j].month == month && getTodos[j].year == year) {
+                const todoContent = document.createElement('div');
+                todoContent.innerHTML = `<p>${getTodos[j].text}</p><p>Tid: ${getTodos[j].timeFrom} - ${getTodos[j].timeTo}</p><div id="icons"><i class="fas fa-times"></i><i class="fas fa-check"></i></div>`;
+                $('.todos').append(todoContent);
+            } 
         }
-        todoList.push(todo);
-        localStorage.setItem(date, JSON.stringify(todoList)); 
-    } else {
-        localStorage.setItem(date, JSON.stringify(Todos.todoList(todo))); 
-    }    
-    
-    showAmountOfTodos();    
-}
-
-const addTodoEventListeners = () => {    
-    $('#icons i:nth-child(1)').click(removeTodo);
-       
-}
-
-const removeTodo = e => {
-    const todo = e.target.parentNode.parentNode;
-    todo.remove();
-}
-
-const markDone = e => {
-    const todo = $(e.target).parent().parent();
-    if (todo.hasClass('done')) {
-        todo.removeClass('done');
-    } else {
-        todo.addClass('done');
     }
-    
 }
 
 const showAmountOfTodos = () => {
@@ -84,6 +59,52 @@ const showAmountOfTodos = () => {
     }  
 }
 
-const showTodos = () => {
-    console.log('hej');
+const newTodo = () => {    
+    let day = $('.date .day-number').text();
+    let month = $('.date .month').text();  
+    let year = $('.date .year').text();
+
+    $('.new-todo').css('display', 'flex');
+    $('.new-todo p').text(`${day} ${month} ${year}`);
+}
+
+const saveTodos = (e) => {
+    e.preventDefault();
+    let text = $('#text').val();
+    let timeFrom = $('#from').val();
+    let timeTo = $('#to').val();
+    
+    let day = $('.date .day-number').text();
+    let month = $('.date .month').text();  
+    let year = $('.date .year').text();
+    let date = day + month + year; 
+    
+    let todoList = [];
+    let todoInStorage; 
+
+    let todo = new Todos(year, month, day, text, timeFrom, timeTo);
+
+    if(localStorage.getItem(date)){
+        let storage = JSON.parse(localStorage.getItem(date));
+        for (let i = 0; i < storage.length; i++){
+            todoInStorage = storage[i];
+            todoList.push(todoInStorage);            
+        }
+        todoList.push(todo);
+        localStorage.setItem(date, JSON.stringify(todoList)); 
+    } else {
+        localStorage.setItem(date, JSON.stringify(Todos.todoList(todo))); 
+    }
+    
+    clearForm();
+    $('.new-todo').css('display', 'none');
+    showAmountOfTodos();
+    $('.todos').html('');
+    showTodos(); 
+}
+
+const closeForm = (e) => {
+    e.preventDefault();
+    clearForm();
+    $('.new-todo').css('display', 'none');
 }
